c19328292c2790ffef422cfabd01b21eb3bb05ff
Uliana Chukanova <uliana9904@yandex.ru>
2024-07-10 14:12:27 +0300

    Создание и удаление новых карточек

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/ulianaChuk/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(data, deleteCallback) {
+  const newCard = cardTemplate.cloneNode(true);
+  //   console.log(newCard);
+  newCard.querySelector(".card__image").src = data.link;
+  newCard.querySelector(".card__title").textContent = data.name;
+  newCard
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCallback);
+
+  return newCard;
+}
 
 // @todo: Функция удаления карточки
-
+function deleteCard() {
+  const button = document.querySelector(".card__delete-button");
+  const deleteButton = button.closest(".places__item");
+  deleteButton.remove();
+}
 // @todo: Вывести карточки на страницу
+function insertCards() {
+  initialCards.forEach((cardData) => {
+    const card = createCard(cardData, deleteCard);
+    placesList.append(card);
+  });
+}
+
+insertCards();
